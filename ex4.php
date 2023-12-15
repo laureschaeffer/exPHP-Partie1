e846b291ff645de5e42805031cac53687331e9ef
laureschaeffer <153603087+laureschaeffer@users.noreply.github.com>
2023-12-15 15:44:51 +0100

    Create ex4.php

---
+++ ex4.php
@@ -0,0 +1,26 @@
+<h1> Exercice 4 </h1>
+
+<p> Ecrire un algorithme permettant de savoir si une phrase est palindrome </p>
+
+<h2> Resultat </h2>
+
+<?php
+
+$phrase="Engage le jeu que je le gagne";
+
+// enlever majuscule
+$phraseMin=strtolower($phrase);
+
+// enlever ' '
+$list = str_replace(' ', '', $phraseMin);
+
+$listInv=strrev($list);
+$conditionVraie = $list == $listInv;
+$conditionFausse = $list!= $listInv;
+
+if ($conditionVraie= True){
+    echo "La phrase « $phrase » est un palindrome.";
+} else {
+    echo "La phrase « $phrase » n'est pas un palindrome.";
+
+}
